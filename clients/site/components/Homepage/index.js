5ab51f0c81037300bbab4d3ffb2d94c047a512fa
Arttu Manninen <arttu@kaktus.cc>
2015-08-01 15:57:00 +0300

    Added mainpage HTML example

--- clients/site/components/Homepage/index.js
+++ clients/site/components/Homepage/index.js
@@ -6,17 +6,21 @@ import { Link } from 'react-router';
 class Homepage extends React.Component {
   render() {
     return (
-      <div>
-        <div className='mainTitle'>
-          <h1>Welcome to home!</h1>
-        </div>
-        <div className='featuredHomes'>
-          <p>
-            <Link to='home' params={{slug: '123'}}>Home 123</Link>
-          </p>
-          <p>
-            <Link to='home' params={{slug: '234'}}>Home 234</Link>
-          </p>
+      <div id='container'>
+        <div className='item big-image fixed ng-scope full-height'>
+          <div className='image-content ng-scope'>
+            <img alt='Live your dream' className='parallax-move show-for-large' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_1920/v1436955483/DSCF9157_bsxil9.jpg' />
+            <img alt='Live your dream' className='parallax-move show-for-medium' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_1000/v1436955483/DSCF9157_bsxil9.jpg' />
+            <img alt='Live your dream' className='parallax-move show-for-small' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_600/v1436955483/DSCF9157_bsxil9.jpg' />
+          </div>
+          <div className='width-wrapper'>
+            <div className='widget large-text' data-vertical='center' data-align='center'>
+              <div>
+                <h1>Homehapp</h1>
+                <p>Discover Y</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
