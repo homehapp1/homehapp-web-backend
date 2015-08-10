7869d5a042fcca79ac098f4f9e9a6bfdde639058
Jerry Jalava <jerry.jalava@qvik.fi>
2015-08-10 16:46:00 +0300

    Staging config

---
+++ config/staging.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function (projectRoot) {
+  var config = {
+    logging: {
+      Console: {
+        enabled: true,
+        level: "debug"
+      }
+    },
+    database: {
+      adapter: "mongoose",
+      adapterConfig: {
+        uri: [
+          "mongodb://homehapp:hSXDuC2VX850FjfQEV+5HFYYrPfw55F0@hhmongo-db-9kx9/homehapp-staging",
+          "mongodb://homehapp:hSXDuC2VX850FjfQEV+5HFYYrPfw55F0@hhmongo-db-9n7m"
+        ],
+        options: {
+          replset: {
+            rs_name: "rs0"
+          }
+        }
+      }
+    }
+  };
+  return config;
+};
