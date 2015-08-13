5392294203c199ca3e478499c109405cf5cc7a22
Arttu Manninen <arttu@kaktus.cc>
2015-08-13 15:20:31 +0300

    Changed the frontpage main image

--- clients/site/components/Homepage/index.js
+++ clients/site/components/Homepage/index.js
@@ -9,13 +9,18 @@ class Homepage extends React.Component {
     window.dispatchEvent(new Event('resize'));
   }
   render() {
+    let images = {
+      small: 'https://images.unsplash.com/photo-1428342628092-61f9e5d578f2?q=80&fm=jpg&s=82762926d328127b919301ae643d63ac',
+      medium: 'https://images.unsplash.com/photo-1428342628092-61f9e5d578f2?q=80&fm=jpg&s=82762926d328127b919301ae643d63ac',
+      large: 'https://images.unsplash.com/photo-1428342628092-61f9e5d578f2?q=80&fm=jpg&s=82762926d328127b919301ae643d63ac'
+    };
     return (
       <div id='container'>
         <div className='item big-image fixed ng-scope full-height'>
           <div className='image-content ng-scope'>
-            <img alt='Live your dream' className='parallax-move show-for-large' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_1920/v1436955483/DSCF9157_bsxil9.jpg' />
-            <img alt='Live your dream' className='parallax-move show-for-medium' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_1000/v1436955483/DSCF9157_bsxil9.jpg' />
-            <img alt='Live your dream' className='parallax-move show-for-small' src='//res.cloudinary.com/kaktus/image/upload/c_scale,q_60,w_600/v1436955483/DSCF9157_bsxil9.jpg' />
+            <img alt='Live your dream' className='parallax-move show-for-large' src={images.small} />
+            <img alt='Live your dream' className='parallax-move show-for-medium' src={images.medium} />
+            <img alt='Live your dream' className='parallax-move show-for-small' src={images.large} />
           </div>
           <div className='width-wrapper'>
             <div className='widget large-text' data-vertical='center' data-align='center'>
