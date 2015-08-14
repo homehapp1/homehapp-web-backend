ae767b659e4d0a53add54e84d0f82bedde659487
Arttu Manninen <arttu@kaktus.cc>
2015-08-14 15:21:48 +0300

    Mainpage graphics

--- clients/site/components/Homepage/index.js
+++ clients/site/components/Homepage/index.js
@@ -8,6 +8,7 @@ class Homepage extends React.Component {
     // Trigger the resize events defined in layout
     window.dispatchEvent(new Event('resize'));
   }
+
   render() {
     let images = {
       small: 'https://images.unsplash.com/photo-1428342628092-61f9e5d578f2?q=80&fm=jpg&s=82762926d328127b919301ae643d63ac',
@@ -15,18 +16,21 @@ class Homepage extends React.Component {
       large: 'https://images.unsplash.com/photo-1428342628092-61f9e5d578f2?q=80&fm=jpg&s=82762926d328127b919301ae643d63ac'
     };
     return (
-      <div id='container'>
-        <div className='item big-image full-height'>
+      <div id='container' className='mainpage'>
+        <div className='item big-image gradient full-height fixed'>
           <div className='image-content' data-vertical='middle'>
             <img alt='Live your dream' className='parallax-move show-for-large' src={images.small} />
             <img alt='Live your dream' className='parallax-move show-for-medium' src={images.medium} />
             <img alt='Live your dream' className='parallax-move show-for-small' src={images.large} />
           </div>
           <div className='width-wrapper'>
-            <div className='widget large-text' data-vertical='center' data-align='center'>
-              <div>
-                <h1>Homehapp</h1>
-                <p>Discover Y</p>
+            <div className='large-text' data-vertical='center' data-align='center'>
+              <div className='splash'>
+                <h1>
+                  <img className='symbol' src='/public/images/homehapp-symbol.svg' alt='' />
+                  <img className='logotype' src='/public/images/homehapp-logotype.svg' alt='Homehapp' />
+                </h1>
+                <p><img className='slogan' src='/public/images/slogan-discovery.svg' alt='Discover y' /></p>
               </div>
             </div>
           </div>
