480c59e9668078946201b96c4f06536751a9f66f
Arttu Manninen <arttu@kaktus.cc>
2015-08-18 11:46:56 +0300

    Use BigImage widget for story page

--- clients/site/components/Neighborhoods/Story.js
+++ clients/site/components/Neighborhoods/Story.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ApplicationStore from '../../../common/stores/ApplicationStore';
 import Gallery from '../../../common/components/Widgets/Gallery';
-//import { Link } from 'react-router';
+import BigImage from '../../../common/components/Widgets/BigImage';
 
 class NeighborhoodsStory extends React.Component {
   constructor() {
@@ -15,11 +15,6 @@ class NeighborhoodsStory extends React.Component {
   render() {
     let galleryImages = ['v1439796815/contentMockup/DSCF9347.jpg', 'v1439796815/contentMockup/DSCF9253.jpg', 'v1439796812/contentMockup/DSCF9310.jpg', 'v1439796810/contentMockup/DSCF9299.jpg', 'v1439796803/contentMockup/DSCF9261.jpg', 'v1439796800/contentMockup/DSCF9339.jpg', 'v1439796799/contentMockup/DSCF9328.jpg', 'v1439796797/contentMockup/DSCF9272.jpg', 'v1439796794/contentMockup/DSCF9301.jpg', 'v1439796791/contentMockup/DSCF9188.jpg', 'v1439796791/contentMockup/DSCF9306.jpg', 'v1439796791/contentMockup/DSCF9280.jpg', 'v1439796780/contentMockup/DSCF9257.jpg', 'v1439796776/contentMockup/DSCF9245.jpg', 'v1439796775/contentMockup/DSCF9201.jpg', 'v1439796764/contentMockup/DSCF9227.jpg', 'v1439796763/contentMockup/DSCF9111.jpg', 'v1439796759/contentMockup/DSCF9158.jpg', 'v1439796753/contentMockup/DSCF9225.jpg', 'v1439796748/contentMockup/DSCF9144.jpg', 'v1439796743/contentMockup/DSCF9178.jpg', 'v1439796741/contentMockup/DSCF9156.jpg', 'v1439796733/contentMockup/DSCF9177.jpg', 'v1439796732/contentMockup/DSCF9160.jpg', 'v1439796719/contentMockup/DSCF9102.jpg', 'v1439796718/contentMockup/DSCF9155.jpg', 'v1439796708/contentMockup/DSCF9141.jpg', 'v1439796701/contentMockup/DSCF9097.jpg', 'v1439796699/contentMockup/DSCF9095.jpg', 'v1439796693/contentMockup/DSCF9108.jpg', 'v1439796687/contentMockup/DSCF9105.jpg', 'v1439796684/contentMockup/DSCF9103.jpg'];
     let imageSrc = galleryImages[0];
-    let images = {
-      small: `${this.config.cloudinary.baseUrl}${this.config.cloudinary.transformations.small}/${imageSrc}`,
-      medium: `${this.config.cloudinary.baseUrl}${this.config.cloudinary.transformations.medium}/${imageSrc}`,
-      large: `${this.config.cloudinary.baseUrl}${this.config.cloudinary.transformations.large}/${imageSrc}`
-    };
 
     let shuffle = function(arr) {
       let currentIndex = arr.length, temporaryValue, randomIndex;
@@ -36,22 +31,14 @@ class NeighborhoodsStory extends React.Component {
     };
 
     galleryImages = shuffle(galleryImages).splice(0, 10);
-    console.log('images', galleryImages);
 
     return (
       <div className='neighborhood-story'>
-        <div className='item big-image full-height fixed' data-gradient='black'>
-          <div className='image-content'>
-            <img alt='Live your dream' className='parallax-move show-for-large' src={images.large} />
-            <img alt='Live your dream' className='parallax-move show-for-medium' src={images.medium} />
-            <img alt='Live your dream' className='parallax-move show-for-small' src={images.small} />
+        <BigImage src={imageSrc} gradient='black' fixed={true}>
+          <div className='large-text' data-vertical='center' data-align='center'>
+            <h1>St. John's wood</h1>
           </div>
-          <div className='width-wrapper full-height'>
-            <div className='large-text' data-vertical='center' data-align='center'>
-              <h1>St. John's wood</h1>
-            </div>
-          </div>
-        </div>
+        </BigImage>
 
         <div className='content-block item'>
           <div className='width-wrapper'>
