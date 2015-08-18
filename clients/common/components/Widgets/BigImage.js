3f672bfa282374ffe3753a17c85f718f86862e35
Arttu Manninen <arttu@kaktus.cc>
2015-08-18 11:46:18 +0300

    Added more properties to BigImage

--- clients/common/components/Widgets/BigImage.js
+++ clients/common/components/Widgets/BigImage.js
@@ -1,9 +1,19 @@
 'use strict';
-import React from 'react';
 
-class BigImage extends React.component {
+import React from 'react';
+import ApplicationStore from '../../stores/ApplicationStore';
+
+class BigImage extends React.Component {
+  constructor() {
+    super();
+    this.config = ApplicationStore.getState().config;
+  }
+
   static propTypes = {
-    src: React.PropTypes.string
+    src: React.PropTypes.string,
+    alt: React.PropTypes.string,
+    fixed: React.PropTypes.boolean,
+    gradient: React.PropTypes.string
   };
 
   render() {
@@ -13,6 +23,12 @@ class BigImage extends React.component {
       'full-height'
     ];
 
+    if (this.props.fixed) {
+      classes.push('fixed');
+    }
+
+    let alt = this.props.alt || '';
+
     let images = {
       small: `${this.config.cloudinary.baseUrl}${this.config.cloudinary.transformations.small}/${this.props.src}`,
       medium: `${this.config.cloudinary.baseUrl}${this.config.cloudinary.transformations.medium}/${this.props.src}`,
@@ -20,11 +36,11 @@ class BigImage extends React.component {
     };
 
     return (
-      <div className={classes.join(' ')}>
+      <div className={classes.join(' ')} data-gradient={this.props.gradient}>
         <div className='image-content'>
-          <img alt='Live your dream' className='parallax-move show-for-large' src={images.large} />
-          <img alt='Live your dream' className='parallax-move show-for-medium' src={images.medium} />
-          <img alt='Live your dream' className='parallax-move show-for-small' src={images.small} />
+          <img alt={alt} className='show-for-large' src={images.large} />
+          <img alt={alt} className='show-for-medium' src={images.medium} />
+          <img alt={alt} className='show-for-small' src={images.small} />
         </div>
         <div className='width-wrapper full-height'>
           {this.props.children}
@@ -33,3 +49,5 @@ class BigImage extends React.component {
     );
   }
 }
+
+export default BigImage;
