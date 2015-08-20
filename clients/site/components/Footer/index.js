a92fd86cefd27e370a5a3c1bcb6609bcc7af6310
Arttu Manninen <arttu@kaktus.cc>
2015-08-20 17:15:54 +0300

    Hide/reveal footer

--- clients/site/components/Footer/index.js
+++ clients/site/components/Footer/index.js
@@ -3,18 +3,50 @@
 import React from 'react';
 import { Link } from 'react-router';
 import ApplicationStore from '../../../common/stores/ApplicationStore';
+import DOMManipulator from '../../../common/DOMManipulator';
 
 class Footer extends React.Component {
   constructor() {
     super();
     this.config = ApplicationStore.getState().config;
+    this.displayFooter = this.displayFooter.bind(this);
+    this.toggleFooter = this.toggleFooter.bind(this);
+    this.hideFooter = this.hideFooter.bind(this);
+  }
+
+  componentDidMount() {
+    this.footer = this.refs.footer.getDOMNode();
+    this.icon = this.refs.footerIcon.getDOMNode();
+
+    this.footer.addEventListener('mouseover', this.displayFooter);
+    this.footer.addEventListener('mouseout', this.hideFooter);
+    this.icon.addEventListener('click', this.toggleFooter);
+    this.icon.addEventListener('touch', this.toggleFooter);
+  }
+
+  toggleFooter() {
+    let footer = new DOMManipulator(this.footer);
+
+    if (footer.hasClass('visible')) {
+      footer.removeClass('visible');
+    } else {
+      footer.addClass('visible');
+    }
+  }
+
+  displayFooter() {
+    (new DOMManipulator(this.footer)).addClass('visible');
+  }
+
+  hideFooter() {
+    (new DOMManipulator(this.footer)).removeClass('visible');
   }
 
   render() {
     return (
-      <div id='footer' className='clearfix'>
+      <div id='footer' className='clearfix' ref='footer'>
         <div className='width-wrapper'>
-          <h3><img src={this.config.revisionedStaticPath + '/images/homehapp-logotype-gray.svg'} alt='Homehapp' /></h3>
+          <h3 ref='footerIcon'><img src={this.config.revisionedStaticPath + '/images/homehapp-symbol.svg'} alt='H' /></h3>
           <ul className='footer-links'>
             <li><Link to='content' params={{slug: 'about'}}>About</Link></li>
             <li><Link to='content' params={{slug: 'terms'}}>Terms & conditions</Link></li>
