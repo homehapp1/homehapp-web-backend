c550e47e7d6df448607e75eea433535cd0c2e469
Arttu Manninen <arttu@kaktus.cc>
2015-08-21 15:15:20 +0300

    Scaffolded store, action and source for home lists

---
+++ clients/site/sources/HomeListSource.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import request from '../../common/request';
+import HomeListActions from '../actions/HomeListActions';
+
+let debug = require('../../common/debugger')('HomeListSource');
+
+let HomeListSource = {
+  fetchHomes: () => {
+    return {
+      remote(storeState) {
+        debug('fetchHomes:remote', arguments);
+        return request.get(`/api/home`)
+          .then((response) => {
+            debug('got response', response);
+            if (!response.data || !response.data.homes) {
+              let err = new Error('Invalid response');
+              return Promise.reject(err);
+            }
+            return Promise.resolve(response.data.homes);
+          })
+          .catch((response) => {
+            if (response instanceof Error) {
+              return Promise.reject(response);
+            } else {
+              let msg = 'unexpected error';
+              if (response.data.error) {
+                msg = response.data.error;
+              }
+              return Promise.reject(new Error(msg));
+            }
+            return Promise.reject(response);
+          });
+      },
+      local(storeState, slug) {
+        return null;
+      },
+      success: HomeListActions.updateHomes,
+      error: HomeListActions.fetchFailed,
+      loading: HomeListActions.fetchHomes
+    };
+  }
+};
+
+export default HomeListSource;
---
+++ clients/site/stores/HomeListStore.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import alt from '../../common/alt';
+import HomeListActions from '../actions/HomeListActions';
+import HomeListSource from '../sources/HomeListSource';
+import Cache from '../../common/Cache';
+
+let debug = require('../../common/debugger')('HomeListStore');
+
+@alt.createStore
+class HomeListStore {
+  constructor() {
+    this.on('bootstrap', () => {
+      debug('bootstrapping', this.homes);
+    });
+
+    this.bindListeners({
+      handleUpdateHomes: HomeListActions.UPDATE_HOMES,
+      handleFetchHomes: HomeListActions.FETCH_HOMES,
+      handleFetchFailed: HomeListActions.FETCH_FAILED
+    });
+
+    this.homes = [];
+    this.error = null;
+
+    this.exportPublicMethods({
+      getHome: this.getHome
+    });
+
+    this.exportAsync(HomeListSource);
+  }
+
+  getHome(id) {
+    debug('getHome', id);
+    let { homes } = this.getState();
+    for (let home of homes) {
+      if (home.id === id) {
+        return home;
+      }
+    }
+    return null;
+  }
+
+  handleUpdateHomes(homes) {
+    debug('handleUpdateHomes', homes);
+    this.homes = homes;
+    this.error = null;
+  }
+  handleFetchHomes() {
+    debug('handleFetchHomes');
+    this.homes = [];
+    this.error = null;
+  }
+  handleFetchFailed(error) {
+    debug('handleFetchFailed', error);
+    this.error = error;
+  }
+}
+
+module.exports = HomeListStore;
