c1284deaa84eddaf1dd760696376064a5639cdf0
Arttu Manninen <arttu@kaktus.cc>
2015-08-21 15:14:27 +0300

    Added a list API route for homes

--- server/routes/site/api/homes.js
+++ server/routes/site/api/homes.js
@@ -5,12 +5,30 @@ import QueryBuilder from '../../../lib/QueryBuilder';
 exports.registerRoutes = (app) => {
   const QB = new QueryBuilder(app);
 
+  app.get('/api/home', function(req, res, next) {
+    console.log('API fetch home');
+    console.log('req.query', req.query);
+
+    QB
+    .query('Home')
+    .parseRequestArguments(req)
+    .findAll()
+    .fetch()
+    .then((result) => {
+      res.json({
+        status: 'ok', homes: result.homes
+      });
+    })
+    .catch(next);
+
+  });
+
   app.get('/api/home/:slug', function(req, res, next) {
     console.log('API fetch home with slug', req.params.slug);
     console.log('req.query', req.query);
 
     QB
-    .query('Home')
+    .forModel('Home')
     .findBySlug(req.params.slug)
     .fetch()
     .then((result) => {
