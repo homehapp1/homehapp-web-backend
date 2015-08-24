cd0c134291e6c8536590797664fe237c13a735ad
Arttu Manninen <arttu@kaktus.cc>
2015-08-24 13:03:48 +0300

    Display story for home details

--- clients/site/components/Home/Details.js
+++ clients/site/components/Home/Details.js
@@ -2,24 +2,17 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import { setFullHeight } from '../../../common/Helpers';
+import Story from '../../../common/components/Widgets/Story';
 
 class HomeDetails extends React.Component {
   static propTypes = {
     home: React.PropTypes.object.isRequired
   }
 
-  componentDidMount() {
-    setFullHeight();
-  }
-
   render() {
+    console.log('home', this.props.home);
     return (
-      <div className='details'>
-        <h1>Home Details for {this.props.home.slug}</h1>
-        <p>{this.props.home.description}</p>
-        <Link to='home' params={{slug: this.props.home.slug}}>Back to story</Link>
-      </div>
+      <Story blocks={this.props.home.story} />
     );
   }
 }
