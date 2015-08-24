2f854b6e01db6936530798b5c1e1ea8b09741d7f
Arttu Manninen <arttu@kaktus.cc>
2015-08-24 15:05:50 +0300

    Use image object url instead of src for BigImage

--- clients/common/components/Widgets/BigImage.js
+++ clients/common/components/Widgets/BigImage.js
@@ -7,8 +7,7 @@ import Image from '../../../common/components/Widgets/Image';
 
 class BigImage extends React.Component {
   static propTypes = {
-    src: React.PropTypes.string,
-    alt: React.PropTypes.string,
+    image: React.PropTypes.string.isRequired,
     fixed: React.PropTypes.bool,
     gradient: React.PropTypes.string,
     proportion: React.PropTypes.number,
@@ -30,15 +29,15 @@ class BigImage extends React.Component {
       classes.push('fixed');
     }
 
-    let alt = this.props.alt || '';
+    let image = this.props.image;
     let proportion = this.props.proportion || 1;
 
     return (
       <div className={classNames(classes)} data-gradient={this.props.gradient} data-proportion={proportion}>
         <div className='image-content'>
-          <Image src={this.props.src} className='show-for-large' alt={alt} variant='large' />
-          <Image src={this.props.src} className='show-for-medium' alt={alt} variant='medium' />
-          <Image src={this.props.src} className='show-for-small' alt={alt} variant='small' />
+          <Image src={image.url} className='show-for-large' alt={image.alt} variant='large' />
+          <Image src={image.url} className='show-for-medium' alt={image.alt} variant='medium' />
+          <Image src={image.url} className='show-for-small' alt={image.alt} variant='small' />
         </div>
         <div className='width-wrapper full-height' data-proportion={proportion}>
           {this.props.children}
