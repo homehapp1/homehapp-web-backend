9c49ffa0023bb4a4c7d662f063406deda753b378
Arttu Manninen <arttu@kaktus.cc>
2015-08-24 15:07:19 +0300

    Typo fix

--- clients/site/components/Home/Story.js
+++ clients/site/components/Home/Story.js
@@ -2,26 +2,17 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import { setFullHeight } from '../../../common/Helpers';
 
 class HomeStory extends React.Component {
   static propTypes = {
     home: React.PropTypes.object.isRequired
   }
 
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    setFullHeight();
-  }
-
   render() {
     return (
       <div className='story'>
         <h1>{this.props.home.title} Story for {this.props.home.slug}</h1>
-        <Link to='homeDetails' params={{slug: this.props.home.slug}}>Got to details</Link>
+        <Link to='homeDetails' params={{slug: this.props.home.slug}}>Go to details</Link>
         <p>
           <Link to='app'>Back to frontpage</Link>
         </p>
