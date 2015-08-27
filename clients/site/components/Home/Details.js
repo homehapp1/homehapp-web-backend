97aaa4c3a614a315f61b4ad4e4c43098dde2b564
Arttu Manninen <arttu@kaktus.cc>
2015-08-27 13:48:43 +0300

    Added default story blocks

--- clients/site/components/Home/Details.js
+++ clients/site/components/Home/Details.js
@@ -10,8 +10,35 @@ class HomeDetails extends React.Component {
   }
 
   render() {
+    let blocks = this.props.home.story.blocks;
+    blocks.push({
+      template: 'Map',
+      properties: {
+        coordinates: this.props.home.location.coordinates
+      }
+    });
+
+    if (this.props.home.location.neighborhood) {
+      blocks.push({
+        template: 'Neighborhood',
+        properties: {
+          title: this.props.home.location.neighborhood
+        }
+      });
+    }
+
+    blocks.push({
+      template: 'Agent',
+      properties: {
+        name: 'Arttu Manninen',
+        title: 'L`ombra dell`estate passata',
+        phone: '+358505958435',
+        email: 'arttu@kaktus.cc'
+      }
+    });
+
     return (
-      <Story blocks={this.props.home.story.blocks} />
+      <Story blocks={blocks} />
     );
   }
 }
