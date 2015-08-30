cb59ac8d9cb8d6dda71578311e89b030af4989ab
Arttu Manninen <arttu@kaktus.cc>
2015-08-31 02:17:15 +0300

    Footer simplified

--- clients/site/components/Footer/index.js
+++ clients/site/components/Footer/index.js
@@ -2,58 +2,17 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import ApplicationStore from '../../../common/stores/ApplicationStore';
-import DOMManipulator from '../../../common/DOMManipulator';
+import Image from '../../../common/components/Widgets/Image';
 
 class Footer extends React.Component {
-  constructor() {
-    super();
-    this.config = ApplicationStore.getState().config;
-    this.displayFooter = this.displayFooter.bind(this);
-    this.toggleFooter = this.toggleFooter.bind(this);
-    this.hideFooter = this.hideFooter.bind(this);
-  }
-
-  componentDidMount() {
-    this.footer = this.refs.footer.getDOMNode();
-    this.icon = this.refs.footerIcon.getDOMNode();
-
-    // this.footer.addEventListener('mouseover', this.displayFooter);
-    // this.footer.addEventListener('mouseout', this.hideFooter);
-    this.icon.addEventListener('click', this.toggleFooter);
-    this.icon.addEventListener('touch', this.toggleFooter);
-  }
-
-  componentWillUnmount() {
-    // this.footer.removeEventListener('mouseover', this.displayFooter);
-    // this.footer.removeEventListener('mouseout', this.hideFooter);
-    this.icon.removeEventListener('click', this.toggleFooter);
-    this.icon.removeEventListener('touch', this.toggleFooter);
-  }
-
-  toggleFooter() {
-    let footer = new DOMManipulator(this.footer);
-
-    if (footer.hasClass('visible')) {
-      footer.removeClass('visible');
-    } else {
-      footer.addClass('visible');
-    }
-  }
-
-  displayFooter() {
-    (new DOMManipulator(this.footer)).addClass('visible');
-  }
-
-  hideFooter() {
-    (new DOMManipulator(this.footer)).removeClass('visible');
-  }
-
   render() {
     return (
       <div id='footer' className='clearfix' ref='footer'>
         <div className='width-wrapper'>
-          <h3 ref='footerIcon'><img src={this.config.revisionedStaticPath + '/images/homehapp-symbol.svg'} alt='H' /></h3>
+          <h3 ref='footerIcon'>
+            <Image src='images/homehapp-symbol.svg' alt='H' type='asset' />
+          </h3>
+          <Image src='images/homehapp-logotype-gray.svg' alt='Homehapp' type='asset' className='logotype' />
           <ul className='footer-links main'>
             <li><Link to='content' params={{slug: 'about'}}>About us</Link></li>
             <li><Link to='content' params={{slug: 'terms'}}>Terms & conditions</Link></li>
@@ -63,17 +22,17 @@ class Footer extends React.Component {
           <ul className='footer-links social'>
             <li>
               <a href='https://www.facebook.com/homehapp' target='_blank'>
-                <img src={this.config.revisionedStaticPath + '/images/icons/facebook.svg'} />
+                <Image src='images/icons/facebook.svg' alt='Facebook' type='asset' />
               </a>
             </li>
             <li>
               <a href='https://www.twitter.com/homehapp' target='_blank'>
-                <img src={this.config.revisionedStaticPath + '/images/icons/twitter.svg'} />
+                <Image src='images/icons/twitter.svg' alt='Twitter' type='asset' />
               </a>
             </li>
             <li>
               <a href='https://www.instagram.com/homehapp' target='_blank'>
-                <img src={this.config.revisionedStaticPath + '/images/icons/instagram.svg'} />
+                <Image src='images/icons/instagram.svg' alt='Instagram' type='asset' />
               </a>
             </li>
           </ul>
