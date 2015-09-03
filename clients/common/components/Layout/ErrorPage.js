bcf134dba32d37a5bb8a3204a016777e00a4b396
Arttu Manninen <arttu@kaktus.cc>
2015-09-03 13:06:00 +0300

    Renamed to yield to JS native Error class

---
+++ clients/common/components/Layout/ErrorPage.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import React from 'react';
+import BigImage from '../Widgets/BigImage';
+import ContentBlock from '../Widgets/ContentBlock';
+import LargeText from '../Widgets/LargeText';
+
+class ErrorPage extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.object,
+    title: React.PropTypes.string.isRequired,
+    message: React.PropTypes.string.isRequired,
+    image: React.PropTypes.object,
+    className: React.PropTypes.string
+  };
+
+  static defaultProps = {
+    className: '',
+    image: {
+      url: 'images/content/not-found.jpg',
+      alt: 'Page not found',
+      type: 'asset'
+    }
+  };
+
+  render() {
+    let classes = [
+      'widget',
+      'large-text',
+      'full-height'
+    ];
+
+    if (this.props.className) {
+      classes.push(this.props.className);
+    }
+
+    let props = {
+      'data-align': this.props.align,
+      'data-valign': this.props.valign
+    };
+
+    return (
+      <div className='widget'>
+        <BigImage image={this.props.image} proportion={0.9}>
+          <LargeText align='center' valign='middle'>
+            <h1>{this.props.title}</h1>
+            <p>{this.props.message}</p>
+          </LargeText>
+        </BigImage>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default ErrorPage;
