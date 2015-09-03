9a3895a947be3ff422bfc121b5f19949d4924091
Arttu Manninen <arttu@kaktus.cc>
2015-09-03 13:25:09 +0300

    Error page to be displayed when a home is not found

--- clients/site/components/ErrorPages/HomeRouteNotFound.js
+++ clients/site/components/ErrorPages/HomeRouteNotFound.js
@@ -1,11 +1,23 @@
 'use strict';
 
+import React from 'react';
+
 import RouteNotFound from './RouteNotFound';
+import ErrorPage from '../../../common/components/Layout/ErrorPage';
+import ContentBlock from '../../../common/components/Widgets/ContentBlock';
 
 class HomeRouteNotFound extends RouteNotFound {
   componentWillMount() {
-    console.log('componentWillMount', this);
+    super.componentWillMount();
     this.error.message = 'Home not found';
+  }
+
+  render() {
+    let error = this.error;
+
+    return (
+      <ErrorPage {...error} />
+    );
   }
 }
 
