4e00e99438c2601409213fce6d8728446487e334
Arttu Manninen <arttu@kaktus.cc>
2015-09-07 14:44:18 +0300

    New <Icon /> widget

---
+++ clients/common/components/Widgets/Icon.js
@@ -0,0 +1,38 @@
+'use strict';
+import React from 'react';
+import Image from './Image';
+
+class Icon extends React.Component {
+  static propTypes = {
+    type: React.PropTypes.string.isRequired,
+    color: React.PropTypes.string,
+    size: React.PropTypes.string,
+    className: React.PropTypes.string
+  };
+
+  static defaultProps = {
+    color: 'black',
+    size: 'medium',
+    className: ''
+  };
+
+  render() {
+    let image = {
+      src: `images/icons/white/${this.props.type}.svg`,
+      alt: '',
+      type: 'asset'
+    };
+
+    let classes = ['widget', 'icon', this.props.color, this.props.size];
+
+    if (this.props.className) {
+      classes.push(this.props.className);
+    }
+
+    return (
+      <span className={classes.join(' ')}><Image {...image} /></span>
+    );
+  }
+}
+
+export default Icon;
