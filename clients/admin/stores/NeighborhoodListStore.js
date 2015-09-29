516fd03a3f5b402c4ece9ade10b461f5544f98ee
Arttu Manninen <arttu@kaktus.cc>
2015-09-29 12:16:09 +0300

    Scaffolded actions and sources for neighborhoods

---
+++ clients/admin/stores/NeighborhoodListStore.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import alt from '../../common/alt';
+import NeighborhoodListActions from '../actions/NeighborhoodListActions';
+import NeighborhoodListSource from '../sources/NeighborhoodListSource';
+// import Cache from '../../common/Cache';
+
+let debug = require('../../common/debugger')('NeighborhoodListStore');
+
+@alt.createStore
+class NeighborhoodListStore {
+  constructor() {
+    this.on('bootstrap', () => {
+      debug('bootstrapping', this.neighborhoods);
+    });
+
+    this.bindListeners({
+      handleUpdateNeighborhoods: NeighborhoodListActions.UPDATE_HOMES,
+      handleFetchNeighborhoods: NeighborhoodListActions.FETCH_HOMES,
+      handleFetchFailed: NeighborhoodListActions.FETCH_FAILED
+    });
+
+    this.neighborhoods = [];
+    this.error = null;
+
+    this.exportPublicMethods({
+      getNeighborhood: this.getNeighborhood
+    });
+
+    this.exportAsync(NeighborhoodListSource);
+  }
+
+  getNeighborhood(id) {
+    debug('getNeighborhood', id);
+    let { neighborhoods } = this.getState();
+    for (let neighborhood of neighborhoods) {
+      if (neighborhood.id === id) {
+        return neighborhood;
+      }
+    }
+    return null;
+  }
+
+  handleUpdateNeighborhoods(neighborhoods) {
+    debug('handleUpdateNeighborhoods', neighborhoods);
+    this.neighborhoods = neighborhoods;
+    this.error = null;
+  }
+  handleFetchNeighborhoods() {
+    debug('handleFetchNeighborhoods');
+    this.neighborhoods = [];
+    this.error = null;
+  }
+  handleFetchFailed(error) {
+    debug('handleFetchFailed', error);
+    this.error = error;
+  }
+}
+
+module.exports = NeighborhoodListStore;
---
+++ clients/admin/stores/NeighborhoodStore.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import alt from '../../common/alt';
+import NeighborhoodActions from '../actions/NeighborhoodActions';
+import NeighborhoodSource from '../sources/NeighborhoodSource';
+// import Cache from '../../common/Cache';
+
+let debug = require('../../common/debugger')('NeighborhoodStore');
+
+@alt.createStore
+class NeighborhoodStore {
+  constructor() {
+    this.on('bootstrap', () => {
+      debug('bootstrapping', this.neighborhood);
+    });
+
+    this.bindListeners({
+      handleCreateItem: NeighborhoodActions.CREATE_ITEM,
+      handleUpdateItem: NeighborhoodActions.UPDATE_ITEM,
+      handleRequestFailed: NeighborhoodActions.REQUEST_FAILED,
+      handleCreateSuccess: NeighborhoodActions.CREATE_SUCCESS,
+      handleUpdateSuccess: NeighborhoodActions.UPDATE_SUCCESS
+    });
+
+    this.error = null;
+
+    this.exportAsync(NeighborhoodSource);
+  }
+
+  handleCreateItem(item) {
+    debug('handleCreateItem', item);
+    this.error = null;
+    if (!this.getInstance().isLoading()) {
+      setTimeout(() => {
+        this.getInstance().createItem(item);
+      });
+    }
+  }
+  handleCreateSuccess(neighborhood) {
+    debug('handleCreateSuccess', neighborhood);
+    this.error = null;
+  }
+
+  handleUpdateItem(item) {
+    debug('handleUpdateItem', item);
+    this.error = null;
+    if (!this.getInstance().isLoading()) {
+      setTimeout(() => {
+        this.getInstance().updateItem(item);
+      });
+    }
+  }
+  handleUpdateSuccess(neighborhood) {
+    debug('handleUpdateSuccess', neighborhood);
+    this.error = null;
+  }
+
+  handleRequestFailed(error) {
+    debug('handleRequestFailed', error);
+    this.error = error;
+  }
+}
+
+module.exports = NeighborhoodStore;
