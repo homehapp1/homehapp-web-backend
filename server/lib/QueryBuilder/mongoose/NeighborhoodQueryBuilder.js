e0c856ef9fc260fe0f6a44741fd5035072878704
Arttu Manninen <arttu@kaktus.cc>
2015-09-30 16:58:38 +0300

    Find all the neighborhoods of a city

--- server/lib/QueryBuilder/mongoose/NeighborhoodQueryBuilder.js
+++ server/lib/QueryBuilder/mongoose/NeighborhoodQueryBuilder.js
@@ -4,6 +4,7 @@ import BaseQueryBuilder from './BaseQueryBuilder';
 import CityQueryBuilder from './CityQueryBuilder';
 import {NotFound} from '../../Errors';
 import async from 'async';
+let debug = require('debug')('app');
 
 class NeighborhoodQueryBuilder extends BaseQueryBuilder {
   constructor(app) {
@@ -13,21 +14,54 @@ class NeighborhoodQueryBuilder extends BaseQueryBuilder {
   initialize() {
   }
 
+  findByCity(city) {
+    if (typeof city.id !== 'undefined') {
+      return this.findByCity(city.id);
+    }
+    this._queries.push((callback) => {
+      this.Model.find({
+        'location.city': city,
+        deletedAt: null
+      }, (err, neighborhoods) => {
+        debug('findByCity', neighborhoods);
+        if (err) {
+          debug('Got error', err);
+          return callback(err);
+        }
+        if (!neighborhoods) {
+          debug('No neighborhoods were found');
+          return callback(new NotFound('Neighborhoods not found'));
+        }
+
+        this._loadedModel = neighborhoods;
+        this.result.models = neighborhoods;
+        this.result.neighborhoods = neighborhoods;
+        callback();
+      });
+    });
+
+    return this;
+  }
+
   findBySlug(slug) {
     this._queries.push((callback) => {
       this.Model.findOne({
         slug: slug,
         deletedAt: null
-      }, (err, model) => {
+      }, (err, neighborhood) => {
+        debug('findBySlug', neighborhood);
         if (err) {
+          debug('Got error', err);
           return callback(err);
         }
-        if (!model) {
-          return callback(new NotFound('neighborhood not found'));
+        if (!neighborhood) {
+          debug('No neighborhood found');
+          return callback(new NotFound('Neighborhood not found'));
         }
-        this.result.neighborhood = model;
-        this.result.neighborhoodJson = model.toJSON();
-        this._loadedModel = model;
+
+        this._loadedModel = neighborhood;
+        this.result.models = [neighborhood];
+        this.result.neighborhood = neighborhood;
         callback();
       });
     });
