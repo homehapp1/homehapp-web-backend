a3fcc2bb53029eac7ec39b41166f30341c30982c
Arttu Manninen <arttu@kaktus.cc>
2015-09-30 17:22:10 +0300

    Fetch homes and populate neighborhoods

--- server/routes/site/api/homes.js
+++ server/routes/site/api/homes.js
@@ -1,9 +1,17 @@
 'use strict';
 
 import QueryBuilder from '../../../lib/QueryBuilder';
+let debug = require('debug')('app');
 
 exports.registerRoutes = (app) => {
   const QB = new QueryBuilder(app);
+
+  let getNeighborhood = function getNeighborhood(id) {
+    return QB
+    .forModel('Neighborhood')
+    .findById(id)
+    .fetch();
+  };
 
   app.get('/api/home/:slug', function(req, res, next) {
     QB
@@ -11,12 +19,20 @@ exports.registerRoutes = (app) => {
     .findBySlug(req.params.slug)
     .fetch()
     .then((result) => {
-      res.json({
-        status: 'ok', home: result.home
-      });
+      debug('home fetched', result);
+      let home = result.home;
+      getNeighborhood(result.home.location.neighborhood)
+      .then((r) => {
+        debug('neighborhood received', r.model);
+        home.location.neighborhood = r.model;
+        res.json({
+          status: 'ok',
+          home: home
+        });
+      })
+      .catch(next);
     })
     .catch(next);
-
   });
 
   app.get('/api/home', function(req, res, next) {
@@ -26,9 +42,41 @@ exports.registerRoutes = (app) => {
     .findAll()
     .fetch()
     .then((result) => {
-      res.json({
-        status: 'ok', homes: result.homes
-      });
+      let homes = result.models.slice(0);
+      let neighborhoods = {};
+
+      let populateNeighborhood = function populateNeighborhood() {
+        // All homes successfully populated, return results
+        if (!homes.length) {
+          res.json({
+            status: 'ok',
+            homes: result.models
+          });
+          return null;
+        }
+
+        let home = homes[0];
+        let id = String(home.location.neighborhood);
+
+        // Neighborhood already fetched, populate with it and move to next
+        if (typeof neighborhoods[id] !== 'undefined') {
+          home.location.neighborhood = neighborhoods[id];
+          homes.shift();
+          return populateNeighborhood();
+        }
+        debug('Neighborhood not found from the list, fetch it');
+
+        getNeighborhood(home.location.neighborhood)
+        .then((r) => {
+          debug('got neighborhood', r.model.title);
+          let _id = String(r.model._id);
+          neighborhoods[_id] = r.model;
+
+          // Populate the neighborhood
+          populateNeighborhood();
+        });
+      };
+      populateNeighborhood();
     })
     .catch(next);
 
