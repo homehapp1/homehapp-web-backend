3ea00ab017dc3dbb4a7dde400dc3bf16feb8709a
Jerry Jalava <jerry.jalava@qvik.fi>
2015-10-01 15:57:06 +0300

    Added Production database config

--- config/production.js
+++ config/production.js
@@ -6,6 +6,23 @@ module.exports = function (projectRoot) {
       Console: {
         enabled: false
       }
+    },
+    database: {
+      adapter: 'mongoose',
+      adapterConfig: {
+        uri: [
+          'mongodb://homehapp:NPUPZZD6Qocrh2o8diDhkXO4KtkXRRmp@hhmongo-db-9kx9/homehapp',
+          'mongodb://homehapp:NPUPZZD6Qocrh2o8diDhkXO4KtkXRRmp@hhmongo-db-9n7m'
+        ],
+        options: {
+          replset: {
+            rs_name: 'rs0'
+          }
+        }
+      }
+    },
+    google: {
+      enabled: true
     }
   };
   return config;
