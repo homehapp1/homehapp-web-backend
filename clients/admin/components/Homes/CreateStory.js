10f9231baffab40eda78bf28ce71c3a18594efea
Arttu Manninen <arttu@kaktus.cc>
2015-10-02 16:14:03 +0300

    Removed unused variables

--- clients/admin/components/Homes/CreateStory.js
+++ clients/admin/components/Homes/CreateStory.js
@@ -1,17 +1,9 @@
 'use strict';
 
 import React from 'react';
-// import { Link } from 'react-router';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Panel from 'react-bootstrap/lib/Panel';
-// import Table from 'react-bootstrap/lib/Table';
-import Input from 'react-bootstrap/lib/Input';
-import Button from 'react-bootstrap/lib/Button';
-import Well from 'react-bootstrap/lib/Well';
-import HomeStore from '../../stores/HomeStore';
-// import HomeActions from '../../actions/HomeActions';
-import StoryEditBlocks from './StoryEditBlocks';
 
 let debug = require('../../../common/debugger')('HomesEditStory');
 
