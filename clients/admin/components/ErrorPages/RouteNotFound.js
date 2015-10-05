19ceb3294f5625aa88d0f47c3dc8692d2b7a4777
Arttu Manninen <arttu@kaktus.cc>
2015-10-05 14:42:55 +0300

    Less humorous output for error pages

--- clients/admin/components/ErrorPages/RouteNotFound.js
+++ clients/admin/components/ErrorPages/RouteNotFound.js
@@ -2,14 +2,13 @@
 
 import React from 'react';
 
-class RouteNotFound extends React.Component {
+export default class RouteNotFound extends React.Component {
   render() {
     return (
       <div className='errorPage'>
-        <h1>Oh No`s! Route does not exist.</h1>
+        <h1>Route does not exist.</h1>
+        <p>The requested route was not found</p>
       </div>
     );
   }
 }
-
-export default RouteNotFound;
