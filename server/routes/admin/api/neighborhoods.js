556199a712b82ac904aea59f3d9348b374f0b23a
Arttu Manninen <arttu@kaktus.cc>
2015-10-05 11:39:05 +0300

    Populate neighborhoods by city

--- server/routes/admin/api/neighborhoods.js
+++ server/routes/admin/api/neighborhoods.js
@@ -2,22 +2,57 @@
 
 import QueryBuilder from '../../../lib/QueryBuilder';
 import {NotImplemented, BadRequest} from '../../../lib/Errors';
+let debug = require('debug')('/api/neighborhoods');
 
 exports.registerRoutes = (app) => {
   const QB = new QueryBuilder(app);
 
   app.get('/api/neighborhoods', function(req, res, next) {
-    console.log('API fetch neighborhoods');
-    console.log('req.query', req.query);
+    debug('API fetch neighborhoods');
+    debug('req.query', req.query);
+    let neighborhoods = [];
+    let cities = {};
 
     QB
     .forModel('Neighborhood')
     .parseRequestArguments(req)
     .findAll()
+    .sort('title')
     .fetch()
     .then((result) => {
+      neighborhoods = result.models;
+      let ids = [];
+      for (let neighborhood of neighborhoods) {
+        if (ids.indexOf(neighborhood.location.city) !== -1) {
+          continue;
+        }
+        ids.push(neighborhood.location.city);
+      }
+
+      return QB
+      .forModel('City')
+      .query({
+        _id: {
+          $in: ids
+        }
+      })
+      .findAll()
+      .fetch();
+    })
+    .then((result) => {
+      debug(`Got ${result.models.length} cities`);
+      for (let city of result.models) {
+        cities[city._id] = city;
+      }
+      for (let neighborhood of neighborhoods) {
+        let id = String(neighborhood.location.city);
+        if (typeof cities[id] !== 'undefined') {
+          neighborhood.location.city = cities[id];
+        }
+      }
       res.json({
-        status: 'ok', neighborhoods: result.neighborhoods
+        status: 'ok',
+        neighborhoods: neighborhoods
       });
     })
     .catch(next);
@@ -34,7 +69,8 @@ exports.registerRoutes = (app) => {
     .fetch()
     .then((result) => {
       res.json({
-        status: 'ok', neighborhood: result.neighborhood
+        status: 'ok',
+        neighborhood: result.neighborhood
       });
     })
     .catch(next);
@@ -57,7 +93,8 @@ exports.registerRoutes = (app) => {
     .update(data)
     .then((model) => {
       res.json({
-        status: 'ok', neighborhood: model
+        status: 'ok',
+        neighborhood: model
       });
     })
     .catch(next);
