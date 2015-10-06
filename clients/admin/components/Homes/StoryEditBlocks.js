34f773e991a33f463384e905f344c9bf1d0a951c
Jerry Jalava <jerry.jalava@qvik.fi>
2015-10-06 17:59:53 +0300

    Hide unworking BigVideo

--- clients/admin/components/Homes/StoryEditBlocks.js
+++ clients/admin/components/Homes/StoryEditBlocks.js
@@ -201,7 +201,6 @@ export default class StoryEditBlocks extends React.Component {
               >
                 <option value=''>Choose template to use</option>
                 <option value='BigImage'>Big Image</option>
-                <option value='BigVideo'>Big Video</option>
                 <option value='ContentBlock'>Content Block</option>
                 <option value='ContentImage'>Content Image</option>
                 <option value='Gallery'>Gallery</option>
