37a56195ab624926cf0634527b6f795ab5649093
Arttu Manninen <arttu@kaktus.cc>
2015-10-08 14:39:08 +0300

    Display debugging data

--- clients/site/components/Home/HomeStoryContainer.js
+++ clients/site/components/Home/HomeStoryContainer.js
@@ -5,6 +5,8 @@ import HomeContainer from './HomeContainer';
 import HomeStore from '../../stores/HomeStore';
 import HomeStory from './HomeStory';
 
+let debug = require('debug')('HomeStoryContainer');
+
 export default class HomeStoryContainer extends HomeContainer {
   render() {
     if (this.state.error) {
@@ -13,6 +15,7 @@ export default class HomeStoryContainer extends HomeContainer {
     if (HomeStore.isLoading() || !this.state.home) {
       return this.handlePendingState();
     }
+    debug('Render', this.state.home);
 
     return (
       <HomeStory home={this.state.home} />
