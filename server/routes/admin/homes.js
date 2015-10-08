2aa13d98813054342eb5f4dcfc81d9ca5030bae6
Arttu Manninen <arttu@kaktus.cc>
2015-10-08 13:01:21 +0300

    URL rewrite public URLs to admin URL space

--- server/routes/admin/homes.js
+++ server/routes/admin/homes.js
@@ -8,66 +8,28 @@ exports.registerRoutes = (app) => {
 
   app.get('/homes', function(req, res, next) {
     return next();
-    // debug('fetch homes');
-    // debug('req.query', req.query);
-    //
-    // QB
-    // .forModel('Home')
-    // .parseRequestArguments(req)
-    // .findAll()
-    // .fetch()
-    // .then((result) => {
-    //   res.locals.data.HomeListStore = {
-    //     homes: result.homesJson
-    //   };
-    //   next();
-    // })
-    // .catch(next);
   });
 
   app.get('/homes/create', function(req, res, next) {
     return next();
-    // debug('Create a blank home');
-    // let model = new (QB.forModel('Home')).Model();
-    // debug('Created a blank', model);
-    // res.locals.data.HomeListStore = {
-    //   homes: [model]
-    // };
-    // next();
   });
 
-  app.get('/homes/edit/:uuid', function(req, res, next) {
-    return next();
-    // debug('Fetch home by uuid', req.params.uuid);
-    // let home = null;
-    //
-    // QB
-    // .forModel('Home')
-    // .findByUuid(req.params.uuid)
-    // .fetch()
-    // .then((result) => {
-    //   home = result.home;
-    //
-    //   return QB
-    //   .forModel('Neighborhood')
-    //   .findById(home.location.neighborhood)
-    //   .fetch();
-    // })
-    // .then((result) => {
-    //   home.location.neighborhood = result.model;
-    //   res.locals.data.HomeListStore = {
-    //     homes: [home]
-    //   };
-    //   next();
-    // })
-    // .catch(() => {
-    //   if (home) {
-    //     res.locals.data.HomeListStore = {
-    //       homes: [home]
-    //     };
-    //   }
-    //   next();
-    // });
+  app.get('/homes/:slug', function(req, res, next) {
+    QB
+    .forModel('Home')
+    .findBySlug(req.params.slug)
+    .fetch()
+    .then((result) => {
+      debug('Redirecting the slug based call to UUID based URL');
+      res.writeHead(301, {
+        Location: `/homes/${result.model.uuid}`
+      });
+      res.end();
+    })
+    .catch(() => {
+      // Home not found by slug, do not redirect
+      next();
+    });
   });
 
 };
