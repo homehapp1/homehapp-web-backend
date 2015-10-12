bd443723bb423a42af8d449d8b11245717bc581e
Arttu Manninen <arttu@kaktus.cc>
2015-10-12 16:16:48 +0300

    Moved slug generator and url name transcoder to common mongoose functions

--- server/schemas/mongoose/common.js
+++ server/schemas/mongoose/common.js
@@ -4,6 +4,10 @@ import Plugins from './Plugins';
 import moment from 'moment';
 import Mongoose from 'mongoose';
 
+let Entities = require('html-entities').Html4Entities;
+let entities = new Entities();
+
+
 exports.getImageSchema = function getImageSchema(Schema) {
   let image = new Schema({
     url: {
@@ -159,4 +163,51 @@ exports.populateMetadata = function populateMetadata(schema) {
     type: Date
   };
   return schema;
+};
+
+exports.urlName = function urlName(str) {
+  let patterns = [
+    ['&amp;', '_et_'],
+    ['&([a-z])(cedil|elig|lpha|slash|tilde|acute|circ|grave|zlig|uml);', '$1'],
+    ['[^0-9a-z\.\-]', '_'],
+    ['^[\\-_\\.]+', ''],
+    ['[\\-_\\.]+$', ''],
+    ['_{2,}', '_'],
+    ['_?\\-_?', '-'],
+    ['\\.{2,}', '.'],
+    ['\\s+', '_']
+  ];
+
+  let tmp = entities.encode(String(str).toLowerCase());
+  let encoded = tmp;
+
+  for (let pattern of patterns) {
+    let regexp = new RegExp(pattern[0], 'ig');
+    encoded = encoded.replace(regexp, pattern[1]);
+  }
+  return encoded;
+};
+
+
+exports.generateUniqueSlug = function generateUniqueSlug(model, cb, iteration) {
+  if (iteration > 10) {
+    return cb(new Error('iteration overflow'));
+  }
+  model.slug = exports.urlName(model.title);
+
+  if (iteration) {
+    model.slug += `-${iteration}`;
+  }
+
+  model.constructor.count({slug: model.slug, deletedAt: null}, function (err, count) {
+    if (err) {
+      return cb(err);
+    }
+    // slug is unique
+    if (count === 0) {
+      return cb();
+    }
+    // slug is not unique
+    generateUniqueSlug(model, cb, (iteration || 0) + 1);
+  });
 };
