a49e13a641ca477efeb9ffec3f61033d49ec14da
Arttu Manninen <arttu@kaktus.cc>
2015-10-13 00:54:17 +0300

    Scaffolded Content page query builder

---
+++ server/lib/QueryBuilder/mongoose/ContentQueryBuilder.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import BaseQueryBuilder from './BaseQueryBuilder';
+import {NotFound} from '../../Errors';
+let debug = require('debug')('ContentQueryBuilder');
+
+export default class ContentQueryBuilder extends BaseQueryBuilder {
+  constructor(app) {
+    super(app, 'Content');
+  }
+
+  findBySlug(slug) {
+    this._queries.push((callback) => {
+      let cursor = this.Model.findOne({
+        slug: slug,
+        deletedAt: null
+      });
+      this._configurePopulationForCursor(cursor);
+      cursor.exec((err, content) => {
+        debug('findBySlug', content.title);
+        if (err) {
+          debug('Got error', err);
+          return callback(err);
+        }
+        if (!content) {
+          debug('No content found');
+          return callback(new NotFound('Content not found'));
+        }
+
+        this._loadedModel = content;
+        this.result.model = content;
+        this.result.models = [content];
+        this.result.content = content;
+        callback();
+      });
+    });
+
+    return this;
+  }
+
+  initialize() {
+  }
+
+  findByUuid(uuid) {
+    this._queries.push((callback) => {
+      let cursor = this.Model.findOne({
+        uuid: uuid,
+        deletedAt: null
+      });
+      this._configurePopulationForCursor(cursor);
+      cursor.exec((err, model) => {
+        if (err) {
+          return callback(err);
+        }
+        if (!model) {
+          return callback(new NotFound('Content page not found'));
+        }
+        this.result.model = model;
+        this.result.models = [model];
+        this.result.content = model;
+        this.result.contentJson = model.toJSON();
+        this._loadedModel = model;
+        callback();
+      });
+    });
+
+    return this;
+  }
+}
