2218a0c36a584509cfab12d167ea345bcd024b1f
Arttu Manninen <arttu@kaktus.cc>
2015-10-15 14:47:37 +0300

    Commented out, since there is no authentication available on the public site yet

--- server/routes/common/api/auth.js
+++ server/routes/common/api/auth.js
@@ -5,17 +5,16 @@ import QueryBuilder from '../../../lib/QueryBuilder';
 exports.registerRoutes = (app) => {
   const QB = new QueryBuilder(app);
 
-  app.get('/api/auth/user', app.authenticatedRoute, function(req, res, next) {
-    QB
-    .forModel('User')
-    .findById(req.user.id)
-    .fetch()
-    .then((result) => {
-      res.json({
-        status: 'ok', user: result.userJson
-      });
-    })
-    .catch(next);
-  });
-
+  // app.get('/api/auth/user', app.authenticatedRoute, function(req, res, next) {
+  //   QB
+  //   .forModel('User')
+  //   .findById(req.user.id)
+  //   .fetch()
+  //   .then((result) => {
+  //     res.json({
+  //       status: 'ok', user: result.userJson
+  //     });
+  //   })
+  //   .catch(next);
+  // });
 };
