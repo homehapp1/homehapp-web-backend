e9bcf62be2aa8ca1c1b3bc0c1bd9b683d6f5b432
Jerry Jalava <jerry.jalava@qvik.fi>
2015-10-16 16:40:23 +0300

    Use BaseClasses

--- clients/admin/actions/AgentListActions.js
+++ clients/admin/actions/AgentListActions.js
@@ -1,29 +1,4 @@
 'use strict';
 
-import alt from '../../common/alt';
-
-let debug = require('../../common/debugger')('AgentListActions');
-
-@alt.createActions
-class AgentListActions {
-  updateItems(items) {
-    this.dispatch(items);
-  }
-  fetchItems(query) {
-    debug('fetchItems', query);
-    this.dispatch(query);
-  }
-  removeItem(id) {
-    debug('removeItem', id);
-    this.dispatch(id);
-  }
-  removeSuccess(id) {
-    this.dispatch(id);
-  }
-  requestFailed(error) {
-    debug('requestFailed', error);
-    this.dispatch(error);
-  }
-}
-
-module.exports = AgentListActions;
+export default require('../../common/actions/BaseListActions')
+  .generate('AgentListActions', {});
