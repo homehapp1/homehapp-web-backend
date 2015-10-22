0906ded416a20d99d5eceb9980e44883a69b298e
Arttu Manninen <arttu@kaktus.cc>
2015-10-22 23:59:36 +0300

    A way to update meta description

--- clients/admin/components/Shared/ViewMetadata.js
+++ clients/admin/components/Shared/ViewMetadata.js
@@ -2,13 +2,45 @@ import React from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Panel from 'react-bootstrap/lib/Panel';
+import Well from 'react-bootstrap/lib/Well';
+import Button from 'react-bootstrap/lib/Button';
 import InputWidget from '../Widgets/Input';
 
 let debug = require('../../../common/debugger')('ViewMetadata');
 
 export default class ViewMetadata extends React.Component {
   static propTypes = {
-    object: React.PropTypes.object.isRequired
+    object: React.PropTypes.object.isRequired,
+    store: React.PropTypes.object,
+    actions: React.PropTypes.object
+  }
+
+  static defaultProps = {
+    store: null,
+    actions: null
+  }
+
+  constructor() {
+    super();
+    this.storeListener = this.onStoreChange.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.store && typeof this.props.store.listen === 'function') {
+      this.props.store.listen(this.storeListener);
+    } else {
+      debug('No store provided for this object, use readonly mode');
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.store && typeof this.props.store.unlisten === 'function') {
+      this.props.store.unlisten(this.storeListener);
+    }
+  }
+
+  onStoreChange(store) {
+    this.setState(store);
   }
 
   createdBy() {
@@ -41,11 +73,45 @@ export default class ViewMetadata extends React.Component {
     );
   }
 
+  onSave() {
+    let data = {
+      id: this.props.object.id,
+      metadata: {
+        description: this.refs.description.getValue()
+      }
+    };
+
+    this.saveMetadata(data);
+  }
+
+  saveMetadata(data) {
+    if (typeof this.props.actions.updateItem === 'function') {
+      debug('Default save metadata called with data', data);
+      this.props.actions.updateItem(data);
+    } else {
+      debug('No actions provided, cannot save');
+    }
+  }
+
   render() {
     if (!this.props.object) {
       return null;
     }
     debug('Metadata object', this.props.object);
+
+    let save = null;
+
+    if (this.props.object.id && this.props.actions) {
+      save = (
+        <Well>
+          <Row>
+            <Col md={6}>
+              <Button bsStyle='success' accessKey='s' onClick={this.onSave.bind(this)}>Save</Button>
+            </Col>
+          </Row>
+        </Well>
+      );
+    }
 
     return (
       <Row>
@@ -66,6 +132,17 @@ export default class ViewMetadata extends React.Component {
               readOnly
               />
           </Panel>
+          <Panel header='Description'>
+            <InputWidget
+              type='textarea'
+              label='Meta description'
+              ref='description'
+              defaultValue={this.props.object.metadata.description}
+              readOnly={(this.props.store) ? false : true}
+              placeholder='A brief description of the object'
+              />
+          </Panel>
+          {save}
         </Col>
       </Row>
     );
