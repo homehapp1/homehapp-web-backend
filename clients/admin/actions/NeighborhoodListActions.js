1d679935f803b0418920927a1c074929b8ee1501
Arttu Manninen <arttu@kaktus.cc>
2015-10-22 16:22:29 +0300

    Reduced debug messages

--- clients/admin/actions/NeighborhoodListActions.js
+++ clients/admin/actions/NeighborhoodListActions.js
@@ -1,9 +1,6 @@
-let debug = require('debug')('NeighborhoodListActions');
-
 export default require('../../common/actions/BaseListActions')
   .generate('NeighborhoodListActions', {
     updateItem(model) {
-      debug('updateItem', model);
       this.dispatch(model);
     }
   });
