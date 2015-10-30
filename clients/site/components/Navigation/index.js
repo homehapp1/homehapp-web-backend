eb379dacb480e0ca4a5acea8a20f08d84a6070b4
Arttu Manninen <arttu@kaktus.cc>
2015-10-30 12:13:27 +0200

    Removed unused vars

--- clients/site/components/Navigation/index.js
+++ clients/site/components/Navigation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import DOMManipulator from '../../../common/DOMManipulator';
 
-let debug = require('debug')('Navigation');
+// let debug = require('debug')('Navigation');
 
 export default class Navigation extends React.Component {
   constructor() {
