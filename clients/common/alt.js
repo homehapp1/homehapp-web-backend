4e6dfcd83f0ff0a0f305fd1fe16e673dd1f055f5
Arttu Manninen <arttu@kaktus.cc>
2015-12-01 18:47:21 +0200

    Custom serializer to eliminate circular references

--- clients/common/alt.js
+++ clients/common/alt.js
@@ -1,7 +1,27 @@
 import Alt from 'alt';
+// import { snapshot } from 'alt/lib/utils/StateFunctions';
 //import chromeDebug from 'alt/utils/chromeDebug';
+const alt = new Alt({
+  serialize: function(data) {
+    // Custom serializer that eliminates circular references
+    let cache = [];
+    let val = JSON.stringify(data, function(key, value) {
+      if (typeof value === 'object' && value !== null) {
+        if (cache.indexOf(value) !== -1) {
+          // Circular reference found, discard key
+          return;
+        }
 
-const alt = new Alt();
+        // Store value in our collection
+        cache.push(value);
+      }
+      return value;
+    });
+    // Send cache to garbage collector
+    cache = [];
+    return val;
+  }
+});
 
 // if (process.env.DEBUG) {
 //   chromeDebug(alt);
