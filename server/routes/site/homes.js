e370ae574adf0853fc05a7b73290cd1d0a6ff198
Arttu Manninen <arttu@kaktus.cc>
2015-12-04 13:23:59 +0200

    Allow using "stories" as a filter keyword

--- server/routes/site/homes.js
+++ server/routes/site/homes.js
@@ -56,11 +56,12 @@ exports.registerRoutes = (app) => {
 
   app.get('/search/:type', function(req, res, next) {
     debug(`GET /search/${req.params.type}`);
-    let types = ['buy', 'rent', 'story'];
+    let types = ['buy', 'rent', 'story', 'stories'];
     let titles = {
       buy: 'Homes for sale',
       rent: 'Homes for rent',
-      story: 'Our storified homes'
+      story: 'Our storified homes',
+      stories: 'Our storified homes'
     };
 
     if (types.indexOf(req.params.type) === -1) {
@@ -115,12 +116,10 @@ exports.registerRoutes = (app) => {
         title: title.join(' | '),
         description: description
       };
-      debug('use home', home.homeTitle);
 
       setLastMod([home, neighborhood], res);
       api.populateCityForHome(home)
       .then(() => {
-        debug('Got home', home.homeTitle);
         res.locals.data.HomeStore = {
           home: home
         };
