d7b6ed7cf84a82fb9f9fc825e4ebac2ecbb49e66
Arttu Manninen <arttu@kaktus.cc>
2015-12-16 14:33:08 +0200

    Prevent falling through to the end when redirecting unauthenticated

--- server/app.js
+++ server/app.js
@@ -424,7 +424,7 @@ exports.run = function(projectName, afterRun) {
               if (app.authenticationRoutes) {
                 let url = encodeURIComponent(req.url);
                 let redirectUrl = `${app.authenticationRoutes.login}?message=${msg}&redirectUrl=${url}`;
-                res.redirect(redirectUrl);
+                res.redirect(302, redirectUrl);
                 return true;
               }
             }
@@ -476,7 +476,7 @@ exports.run = function(projectName, afterRun) {
           if (isJSONRequest) {
             payload = prepareJSONError();
           } else if (handleUnauthenticatedGetRequest()) {
-            return next();
+            return resolve();
           }
 
           if (err.stack && app.config.env !== 'production') {
