c0ed37a9d3289f6dd8fe3a50f4950f3878be76bd
Arttu Manninen <arttu@kaktus.cc>
2015-12-16 13:23:04 +0200

    Get the application store for the main layout

--- clients/common/components/Layout/index.js
+++ clients/common/components/Layout/index.js
@@ -1,12 +1,18 @@
 /* global window */
 
-
 import React from 'react';
 import { scrollTop, setFullHeight, itemViews } from '../../Helpers';
+import ApplicationStore from '../../stores/ApplicationStore';
+
+let debug = require('debug')('Layout');
 
 export default class Layout extends React.Component {
   static propTypes = {
-    children: React.PropTypes.array.isRequired
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.array,
+      React.PropTypes.object,
+      React.PropTypes.null
+    ]).isRequired
   }
 
   static defaultProps = {
@@ -15,14 +21,26 @@ export default class Layout extends React.Component {
   constructor() {
     super();
     this.pageScroller = this.pageScroller.bind(this);
+    this.stateListener = this.onStateChange.bind(this);
+  }
+
+  state = {
+    application: ApplicationStore.getState()
   }
 
   componentDidMount() {
+    debug('ApplicationStore', this.state.application, ApplicationStore);
     window.addEventListener('resize', setFullHeight);
     window.addEventListener('scroll', itemViews);
 
     this.refs.scroller.getDOMNode().addEventListener('click', this.pageScroller, true);
     this.refs.scroller.getDOMNode().addEventListener('touchstart', this.pageScroller, true);
+
+    ApplicationStore.listen(this.stateListener);
+
+    if (!this.state.application) {
+      ApplicationStore.getState();
+    }
 
     // Trigger the events on load
     setFullHeight();
@@ -34,8 +52,14 @@ export default class Layout extends React.Component {
     window.removeEventListener('scroll', itemViews);
     this.refs.scroller.getDOMNode().removeEventListener('click', this.pageScroller);
     this.refs.scroller.getDOMNode().removeEventListener('touchstart', this.pageScroller);
+
+    ApplicationStore.unlisten(this.stateListener);
   }
 
+  onStateChange(state) {
+    debug('got state', state);
+    this.setState(state);
+  }
 
   pageScroller() {
     let top = scrollTop() + window.innerHeight;
