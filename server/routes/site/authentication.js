2f91e81f2ab0b0272853ea5c9521f8bd82f20269
Arttu Manninen <arttu@kaktus.cc>
2015-12-17 13:45:57 +0200

    Pass the authentication URL to React

--- server/routes/site/authentication.js
+++ server/routes/site/authentication.js
@@ -1,31 +1,7 @@
-// let debug = require('debug')('/auth/login');
+let debug = require('debug')('/auth/login');
 
 exports.registerRoutes = (app) => {
   app.get('/auth/login', function(req, res, next) {
-    let redirectTo = '/';
-
-    if (req.query.redirectTo) {
-      redirectTo = req.query.redirectTo;
-    }
-
-    if (req.body.redirectTo) {
-      redirectTo = req.body.redirectTo;
-    }
-
-    app.getLocals(req, res, {
-      includeClient: false,
-      bodyClass: 'adminLogin',
-      csrfToken: req.csrfToken(),
-      redirectUrl: redirectTo
-    })
-    .then((locals) => {
-      //locals.layout = null;
-      // res.render('login', locals);
-      let data = {
-        csrfToken: locals.csrfToken,
-        redirectUrl: locals.redirectUrl
-      };
-      next();
-    });
+    next();
   });
 };
