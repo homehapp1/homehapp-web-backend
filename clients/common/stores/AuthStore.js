09e9e5155bce96c18bb96b25136500938841f67e
Arttu Manninen <arttu@kaktus.cc>
2015-12-19 13:52:24 +0200

    Removed debug data

--- clients/common/stores/AuthStore.js
+++ clients/common/stores/AuthStore.js
@@ -2,7 +2,7 @@ import alt from '../../common/alt';
 import AuthActions from '../actions/AuthActions';
 import AuthSource from '../sources/AuthSource';
 
-let debug = require('../debugger')('AuthStore');
+// let debug = require('../debugger')('AuthStore');
 
 @alt.createStore
 class AuthStore {
@@ -21,18 +21,16 @@ class AuthStore {
   }
 
   handleUpdateUser(user) {
-    debug('handleUpdateUser', user);
     this.user = user;
     this.error = null;
   }
-  handleFetchUser() {
-    debug('handleFetchUser');
+  handleFetchUser(args) {
     this.user = null;
     this.error = null;
   }
   handleFetchFailed(error) {
-    debug('handleFetchFailed', error);
     this.error = error;
+    this.user = null;
   }
 }
 
