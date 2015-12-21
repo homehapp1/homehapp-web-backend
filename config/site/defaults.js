1b95c09cf1953fec9da76bc47e53e9f0fe414ec3
Arttu Manninen <arttu@kaktus.cc>
2015-12-21 11:14:56 +0200

    Use single quotes instead of double quotes for the sake of consistency

--- config/site/defaults.js
+++ config/site/defaults.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
-import {getEnvironmentValue} from "../../server/lib/Helpers";
+import {getEnvironmentValue} from '../../server/lib/Helpers';
 
 module.exports = (projectRoot) => {
-  let env = getEnvironmentValue("NODE_ENV", "development");
-  let port = getEnvironmentValue("NODE_PORT", 3001);
-  let hostname = getEnvironmentValue("HOSTNAME", "localhost");
+  let env = getEnvironmentValue('NODE_ENV', 'development');
+  let port = getEnvironmentValue('NODE_PORT', 3001);
+  let hostname = getEnvironmentValue('HOSTNAME', 'localhost');
   let host = `http://${hostname}:${port}`;
 
   let config = {
     authentication: {
-      adapters: ["local"],
+      adapters: ['local'],
       adapterConfigs: {
         local: {
           cookie: {
             maxAge: 86000
           },
           session: {
-            name: "homehapp:login",
-            secret: "really-secret-string-here"
+            name: 'homehapp:login',
+            secret: 'really-secret-string-here'
           }
         }
       }
