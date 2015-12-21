a9378a17ad38e9f895aa7633e4d6d61581cb8264
Arttu Manninen <arttu@kaktus.cc>
2015-12-21 12:59:17 +0200

    API test environment

---
+++ test/api/utils.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import app from '../../server/app';
+
+const TEST_USER_DATA = exports.TEST_USER_DATA = {
+  email: 'test@qvik.fi',
+  username: 'test',
+  password: 'test'
+};
+
+let createApp = exports.createApp = (done) => {
+  app.run('api', (app) => done(null, app));
+};
