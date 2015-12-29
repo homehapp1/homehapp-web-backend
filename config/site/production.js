8baab8f46c45a8385870395565fa29806ebba305
Arttu Manninen <arttu@kaktus.cc>
2015-12-29 06:04:29 +0200

    Use local authentication instead of HTTP basic authentication on production and staging

--- config/site/production.js
+++ config/site/production.js
@@ -3,7 +3,18 @@
 module.exports = function (projectRoot) {
   var config = {
     authentication: {
-      adapters: ['basic']
+      adapters: ['local'],
+      adapterConfigs: {
+        local: {
+          cookie: {
+            maxAge: 86000
+          },
+          session: {
+            name: 'homehapp:login',
+            secret: 'really-secret-string-here'
+          }
+        }
+      }
     }
   };
   return config;
