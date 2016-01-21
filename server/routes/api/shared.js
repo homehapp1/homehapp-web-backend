1e81de93bd0f063c54d9fd8c9ebb6dce6a3aa07b
Arttu Manninen <arttu@kaktus.cc>
2016-01-21 15:11:21 +0200

    Added specific examples for images and videos

--- server/routes/api/shared.js
+++ server/routes/api/shared.js
@@ -1,11 +1,22 @@
 exports.registerRoutes = (app) => {
   /**
+   * @apiDefine ImageSuccess
+   * @apiVersion 1.0.1
+   *
+   * @apiSuccess {String} url     Image URL
+   * @apiSuccess {String} alt     Brief image description
+   * @apiSuccess {Number} width   Width of the image
+   * @apiSuccess {Number} height  Height of the image
+   * @apiSuccess {Number} aspectRatio Width/height ratio of the image
+   */
+
+  /**
    * @apiDefine ImageSuccessJSON
    * @apiVersion 1.0.1
    *
    * @apiSuccessExample {json} Image JSON
    *     {
-   *       "url": "...",
+   *       "url": "https://...",
    *       "alt": "...",
    *       "width": ...,
    *       "height": ...,
@@ -16,11 +27,28 @@ exports.registerRoutes = (app) => {
    */
 
   /**
+   * @apiDefine ImageRequestJSON
+   * @apiVersion 1.0.1
+   * @apiParamExample {json} Request-Example
+   *    {
+   *      "url": "https://...",
+   *      "alt": "...",
+   *      "width": 1920,
+   *      "height": 1080,
+   *      "fixed": false,
+   *      "align": "center",
+   *      "valign": "middle"
+   *    }
+   */
+
+  /**
    * @api {any} /api/* Images
    * @apiVersion 1.0.1
    * @apiName  Images
    * @apiGroup Shared
+   * @apiUse ImageSuccess
    * @apiUse ImageSuccessJSON
+   * @apiUse ImageRequestJSON
    *
    * @apiDescription Every Image uses the following definition
    *
@@ -43,6 +71,11 @@ exports.registerRoutes = (app) => {
    * @apiSuccess {Number} width   Width of the video
    * @apiSuccess {Number} height  Height of the video
    * @apiSuccess {Array} derived  An array of derived video objects
+   * @apiSuccess {Object} derived.0       An example of a derived video object
+   * @apiSuccess {String} derived.0.url     Derived video URL
+   * @apiSuccess {String} derived.0.format  Derived video format
+   * @apiSuccess {Number} derived.0.width   Derived video width
+   * @apiSuccess {Number} derived.0.height  Derived video height
    */
 
   /**
@@ -51,24 +84,54 @@ exports.registerRoutes = (app) => {
    *
    * @apiSuccessExample {json} Video JSON
    *     {
-   *       "url": "...",
+   *       "url": "https://...",
    *       "alt": "...",
    *       "width": ...,
    *       "height": ...,
    *       "derived": [
    *         {
-   *           "url": "...",
+   *           "url": "https://...",
    *           "width": ...,
-   *           "height": ...
+   *           "height": ...,
+   *           "format": "webm"
    *         },
    *         ...,
    *         {
-   *           "url": "...",
+   *           "url": "https://...",
    *           "width": ...,
-   *           "height": ...
+   *           "height": ...,
+   *           "format": "mp4"
    *         }
    *       ]
    *     }
+   */
+
+  /**
+   * @apiDefine VideoRequestJSON
+   * @apiVersion 1.0.1
+   * @apiParamExample {json} Request-Example
+   *    {
+   *      "url": "https://...",
+   *      "alt": "...",
+   *      "width": 1920,
+   *      "height": 1080,
+   *      "fixed": false,
+   *      "align": "center",
+   *      "valign": "middle",
+   *      "derived": [
+   *         {
+   *           "url": "https://...",
+   *           "width": ...,
+   *           "height": ...,
+   *           "format": "webm"
+   *         },
+   *         {
+   *           "url": "https://...",
+   *           "width": ...,
+   *           "height": ...,
+   *           "format": "mp4"
+   *      ]
+   *    }
    */
 
   /**
@@ -78,13 +141,20 @@ exports.registerRoutes = (app) => {
    * @apiGroup Shared
    * @apiUse VideoSuccess
    * @apiUse VideoSuccessJSON
+   * @apiUse VideoRequestJSON
    *
    * @apiDescription Every Video uses the following definition
    *
-   * @apiParam {String} url        Video URL
-   * @apiParam {String} [alt]        Alt text or brief description
-   * @apiParam {Number} [width]      Video width
-   * @apiParam {Number} [height]     Video height
-   * @apiParam {boolean} [fixed]     Flag for fixing the video position for parallax scrolling
+   * @apiParam {String} url         Video URL
+   * @apiParam {String} [alt]       Alt text or brief description
+   * @apiParam {Number} [width]     Video width
+   * @apiParam {Number} [height]    Video height
+   * @apiParam {boolean} [fixed]    Flag for fixing the video position for parallax scrolling
+   * @apiParam {Array} [derived]        Derived videos
+   * @apiParam {Object} [derived.0]     Derived video object, which - when presented - has to fulfill its required fields
+   * @apiParam {String} derived.0.video.url         Derived video URL
+   * @apiParam {String} derived.0.video.format      Derived video format
+   * @apiParam {Number} [derived.0.video.width]     Derived video width
+   * @apiParam {Number} [derived.0.video.height]    Derived video height
    */
 };
